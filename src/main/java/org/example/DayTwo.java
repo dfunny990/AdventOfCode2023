2d44f8b357b01dc55199ee1f37a395a922ff4041
Patrick Saperstein <patricksaperstein@gmail.com>
2023-12-05 10:52:17 -0500

    Day Two Part two

--- src/main/java/org/example/DayTwo.java
+++ src/main/java/org/example/DayTwo.java
@@ -12,45 +12,49 @@ import static java.lang.Integer.parseInt;
 public class DayTwo {
     public static int RunDayTwo(){
         int total = 0;
-        int counter = 1;
         List<String> input = Utils.GetStringListFromFile("DayTwoInput.txt");
         for (String line : input) {
             int colonIndex = line.indexOf(":");
             line = line.substring(colonIndex+2);
-            if (ProcessGame(line)) {
-                total += counter;
-            }
-            counter ++;
+            total += ProcessGame(line);
         }
         return total;
     }
 
-    private static boolean ProcessGame(String line) {
+    private static int ProcessGame(String line) {
         String[] shows = line.split(";");
+        int maxRed = 0;
+        int maxGreen = 0;
+        int maxBlue = 0;
+
         for (String show : shows) {
             List<String> dice = Arrays.asList(show.split(","));
-            boolean validShow = ProcessDice(dice);
-            if (!validShow) {
-                return false;
-            }
+            int[] requiredDiceRGB = ProcessDice(dice);
+            maxRed = Math.max(maxRed, requiredDiceRGB[0]);
+            maxGreen = Math.max(maxGreen, requiredDiceRGB[1]);
+            maxBlue = Math.max(maxBlue, requiredDiceRGB[2]);
         }
-        return true;
+        return maxRed * maxGreen * maxBlue;
     }
 
-    private static boolean ProcessDice(List<String> dice) {
+    private static int[] ProcessDice(List<String> dice) {
+        int[] rgbValues = new int[3];
         for (String roll : dice) {
             roll = roll.trim();
-            System.out.println(roll);
             int spaceIndex = roll.indexOf(" ");
             int numDice = parseInt(roll.substring(0,spaceIndex));
             String color = roll.substring(spaceIndex+1);
-            if ((color.equals("red") && numDice > 12) ||
-                (color.equals("green") && numDice > 13) ||
-                (color.equals("blue") && numDice > 14)) {
-                return false;
+            if (color.equals("red")) {
+                rgbValues[0] = numDice;
+            }
+            if (color.equals("green")) {
+                rgbValues[1] = numDice;
+            }
+            if (color.equals("blue")){
+                rgbValues[2] = numDice;
             }
         }
-        return true;
+        return rgbValues;
     }
 
 }
